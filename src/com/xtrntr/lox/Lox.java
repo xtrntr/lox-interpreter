38ad93add4d9d360cad7ae696de779ce7f0042bb
xtrntr <kangren.chia@gmail.com>
2021-07-18 18:06:09 +0800

    chapter 6

--- src/com/xtrntr/lox/Lox.java
+++ src/com/xtrntr/lox/Lox.java
@@ -47,10 +47,18 @@ public class Lox {
     Scanner scanner = new Scanner(source);
     List<Token> tokens = scanner.scanTokens();
 
-    // For now, just print the tokens.
-    for (Token token : tokens) {
-      System.out.println(token);
-    }
+    Parser parser = new Parser(tokens);
+    Expr expression = parser.parse();
+
+    // Stop if there was a syntax error.
+    if (hadError) return;
+
+    System.out.println(new AstPrinter().print(expression));
+
+    // // For now, just print the tokens.
+    // for (Token token : tokens) {
+    //   System.out.println(token);
+    // }
   }
 
   static void error(int line, String message) {
@@ -62,7 +70,12 @@ public class Lox {
     hadError = true;
   }
 
-  
-
+  static void error(Token token, String message) {
+    if (token.type == TokenType.EOF) {
+      report(token.line, " at end", message);
+    } else {
+      report(token.line, " at '" + token.lexeme + "'", message);
+    }
+  }
 }
 
